ca3c0bbf7c8f524d0ef7c886d6ddab89eea374b0
EnzoGomez03 <enzolucha@gmail.com>
2026-07-13 16:08:02 -0300

    Cambio de imagenes

--- js/productos.js
+++ js/productos.js
@@ -3,24 +3,24 @@ const productos = [
         id: 1,
         nombre: "Conitos de dulce de leche",
         precio: 6500,
-        imagen: "assets/img/Conitos.jpeg"
+        imagen: "assets/img/conitosFatu.jpeg"
     },
     {
         id: 2,
         nombre: "Alfajores de maicena",
         precio: 7000,
-        imagen: "assets/img/alfajores.jpeg"
+        imagen: "assets/img/alfajoresFatu.jpeg"
     },
     {
         id: 3,
         nombre: "Palmeritas",
         precio: 5500,
-        imagen: "assets/img/palmeritas.jpeg"
+        imagen: "assets/img/palmeritasFatu.jpeg"
     },
     {
         id: 4,
         nombre: "Rosquitas",
         precio: 5000,
-        imagen: "assets/img/rosquitas.jpeg"
+        imagen: "assets/img/rosquitasFatu.jpeg"
     }
 ];
